07676a9d34a06c9697e1dfd4c2e5509ea2f1f357
jaber111 <34353274+jaber111@users.noreply.github.com>
2020-09-16 11:52:47 +0100

    manip some manip in width

--- src/index.js
+++ src/index.js
@@ -2,19 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
 import { mediaQueries } from "./mediaQueries";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Box = styled.div`
-  background-color: red;
-  width: 200px;
+  /* background-color: red; */
+  /* width: 200px; */
   height: 200px;
-  ${mediaQueries("md")`
-    background-color: green;
+    /* ${mediaQueries("lg")`
+   min-width:1000px;
+  `}; */
+    ${mediaQueries("md")`
+   max-width:10px;
+   display:flex;
   `};
-  ${props => mediaQueries("md")(`width: ${props.width}px;`)}
+    ${mediaQueries("sm")`
+   max-width:10px;
+   display:flex;
+  `};
+      ${mediaQueries("pg")`
+   max-width:10px;
+   display:flex;
+  `};
+  /* ${(props) => mediaQueries("md")(`width: ${props.width}px;`)} */
+  ${(props) => ` background-color: ${props.color};`}
+  ${(props) => ` height: ${props.height}px;`}
+
 `;
 
 function App() {
-  return <Box width={400} />;
+  return (
+    <Row className="border border-dark">
+      <Col md={6} className="center-block">
+        <Box color={"red"} height={300} />
+      </Col>
+      <Col md={6} className="center-block">
+        <Box color={"blue"} height={300} />
+      </Col>
+    </Row>
+  );
 }
 
 const rootElement = document.getElementById("root");
--- src/mediaQueries.js
+++ src/mediaQueries.js
@@ -2,7 +2,8 @@ export const breakpoints = {
   sm: "320px",
   md: "480px",
   lg: "720px",
-  xl: "960px"
+  xl: "960px",
+  pg: "0px"
 };
 
 export const mediaQueries = (key) => {
